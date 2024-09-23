689f61f9eac4e10ca59fb2f22a2707c81f8692b9
italloramon <itallostudy@gmail.com>
2024-09-23 08:56:39 -0300

    chore: mapping Category to entity

--- src/main/java/com/italloramon/dscatalog/entities/Category.java
+++ src/main/java/com/italloramon/dscatalog/entities/Category.java
@@ -1,10 +1,18 @@
 package com.italloramon.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Category() {}
